4114241b8ac9dc61c0de8dfde3ddc7eeb9bd3202
Monov Ray <65419706+ray-mov@users.noreply.github.com>
2023-12-16 06:37:11 +0000

    custom css module added to ClockHeading component of BharatClock project

--- react-redux-tutorial/src/bharat-clock/ClockHeading.jsx
+++ react-redux-tutorial/src/bharat-clock/ClockHeading.jsx
@@ -1,5 +1,8 @@
+import styles from"./ClockHeading.module.css"
+console.log(styles.ClockHeading)
+
 let ClockHeading = () => {
-return <h1 class="fw-bold fs-1">Bharat Clock</h1>
+return <h1 class={`${styles["clock-header"]} fw-bold fs-1v`}>Bharat Clock</h1>
 }
 
 export default ClockHeading
